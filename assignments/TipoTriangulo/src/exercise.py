d6888b13d896bb1293d750c8df4bf48e39261f1e
Diego0103 <A01254073@itesm.mx>
2021-09-03 04:21:28 +0000

    TipoTriangulo

---
+++ assignments/TipoTriangulo/src/exercise.py
@@ -0,0 +1,12 @@
+def main():
+    #escribe tu código abajo de esta línea
+    x=int(input("X: "))
+    y=int(input("Y: "))
+    z=int(input("Z: "))
+    if x==y and y==z:
+        print ("Equilatero")
+    elif 
+
+
+if __name__=='__main__':
+    main()
