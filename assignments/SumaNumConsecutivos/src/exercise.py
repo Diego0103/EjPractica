536f24e9f8e1b07b0eae3545e862f2d527be0937
Diego0103 <A01254073@itesm.mx>
2021-09-07 17:27:14 +0000

    SumaNumConsecutivos

---
+++ assignments/SumaNumConsecutivos/src/exercise.py
@@ -0,0 +1,23 @@
+#Escribe un método que reciba un número entero positivo n, después debe calcular la suma 1+2+3+...+n. 
+#Finalmente regrese el resultado de la suma y sea impreso en pantalla.
+#Entrada
+#Un número entero positivo n
+#Salida
+#El resultado de la suma 1+2+3...+n
+#Ejemplo de ejecución del programa:
+#>>>6
+#21
+def main():
+    #escribe tu código abajo de esta línea
+    n=int(input("Dame un número: ")
+    suma=0
+    if n>0:
+        for i in range(1,n+1):
+            
+
+
+
+    
+
+if __name__=='__main__':
+    main()
