1e22c9743ec395a86d08ac94d47e2121ddcb4ab5
Diego0103 <A01254073@itesm.mx>
2021-09-10 06:15:04 +0000

    CantLitrosPintura

--- assignments/CantLitrosPintura/src/exercise.py
+++ assignments/CantLitrosPintura/src/exercise.py
@@ -1,3 +1,16 @@
+#Litros de pintura
+#Escribe un programa para calcular la cantidad de litros de pintura necesarios para pintar una superficie.
+#El programa debe leer el área de la superficie a pintar en metros cuadrados y la cantidad de metros cuadrados que se pueden cubrir con un litro de pintura. Y mostrar la cantidad de litros de pintura que se deben comprar para cubrir esa superficie.
+#Considera que sólo se pueden comprar litros completos de pintura.
+#Entradas:
+#El área a pintar (número flotante)
+#La cantidad de metros cuadrados que se pueden cubrir con un litro de pintura (número flotante)
+#Salida
+#La cantidad de litros a pintar (número entero)
+#Ejemplo de ejecución del programa:
+#>>> 857.5
+#>>> 10
+#86
 import math
 def main():
     #escribe tu código abajo de esta línea
