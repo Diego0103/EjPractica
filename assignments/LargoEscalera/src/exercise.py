b5ba4b0c8058f10ba1d12f50d48d4ab5bb9b7382
Diego0103 <A01254073@itesm.mx>
2021-09-10 06:16:46 +0000

    LargoEscalera

--- assignments/LargoEscalera/src/exercise.py
+++ assignments/LargoEscalera/src/exercise.py
@@ -1,3 +1,13 @@
+#Largo de una escalera 
+#Escribe un programa que determine el largo que debe tener una escalera, la cual se necesita para alcanzar una altura determinada, cuando se pone contra una casa. La altura que se desea alcanzar y el ángulo que deberá hacer la escalera contra la pared, son datos que te proporciona el usuario. Para calcular el largo de la escalera usa la siguiente ecuación: largo=altura/seno(ángulo). Recuerda que las funciones de Python que calculan seno y coseno, reciben los ángulos en radianes, pero tu programa va a recibir los ángulos en grados.
+#Entrada
+#Un número que corresponde a la altura de la casa (flotante positivo) y el ángulo en grados (entero positivo), en ese orden.
+#Salida
+#Un número, un número que representa el largo que debe tener la escalera. IMPORTANTE: Redondea el número para que el resultado sea entero. Utiliza la función adecuada que te provee Python para realizar un redondeo.
+#Ejemplo:
+#>>> 2
+#>>> 45
+#3
 import math
 def main():
     #escribe tu código abajo de esta línea
