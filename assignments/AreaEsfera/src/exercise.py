8ffdbb27106256dc210e23f52ec6611cb3a86443
Diego0103 <A01254073@itesm.mx>
2021-09-10 06:12:56 +0000

    AreaEsfera

--- assignments/AreaEsfera/src/exercise.py
+++ assignments/AreaEsfera/src/exercise.py
@@ -1,3 +1,13 @@
+#Ejercicio 1
+#El área y volumen de una esfera se calcula con las siguientes fórmulas:
+#área =  4πr2
+#volumen = 4πr33
+#Escribe un programa que pida el valor del radio y muestre su área y su volumen.
+#Ejercicio 2
+#El área de un triángulo cuyos lados son de longitud a, b y c se calcula como 
+#área = s(s−a)(s−b)(s−c)−−−−−−−−−−−−−−−−−−√2          
+#dónde s=(a+b+c)2
+#Escribe un programa que pida al usuario los valores a, b y c y calcule y muestre el área del triángulo usando esta fórmula.
 import math
 def main():
     #escribe tu código abajo de esta línea
